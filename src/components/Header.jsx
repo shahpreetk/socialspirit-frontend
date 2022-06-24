71b626d59c2ac4a3bc616e56554ae7abe5aa8ce7
Preet Shah <shahpreetk@gmail.com>
2022-06-24 16:18:32 +0100

    header made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { FaBars, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      {/* <header className="header">
-       <div className="logo">
-         <Link to="/">Social Spirit</Link>
-       </div>
-       <ul>
-         <li><Link to="/login"><FaSignInAlt/>Login</Link></li>
-         <li><Link to="/register"><FaUser/>Register</Link></li>
-       </ul>
-     </header> */}
-      <header className="navbar bg-base-200">
-        <div className="flex-1">
+      <header className="navbar bg-base-200 h-20">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
+              <FaBars size={18} />
+            </label>
+            <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><Link to="/login"><FaSignInAlt />Login</Link></li>
+              <li><Link to="/register"><FaUser />Register</Link></li>
+            </ul>
+          </div>
           <Link to="/" className="btn btn-ghost normal-case text-xl">Social Spirit</Link>
         </div>
-        <div className="flex-none">
+        <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
             <li><Link to="/login"><FaSignInAlt />Login</Link></li>
             <li><Link to="/register"><FaUser />Register</Link></li>
