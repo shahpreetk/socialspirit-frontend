1a8c12d7a7c18daeecb85ca83f85e923a9c53ada
Preet Shah <shahpreetk@gmail.com>
2022-07-22 10:10:21 +0100

    home page title & search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { V_PROFILE, O_PROFILE } from '../constants/routes';
+import { IoSearch } from "react-icons/io5";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,7 +16,7 @@ const Home = () => {
         navigate(O_PROFILE);
       }
     }
-    if (localStorage.getItem('volunteer')){
+    if (localStorage.getItem('volunteer')) {
       if (!volunteer.introduction || !volunteer.hobbies.length) {
         navigate(V_PROFILE);
       }
@@ -24,7 +25,22 @@ const Home = () => {
 
   return (
     <>
-      <div> Home</div>
+      <div className="hero min-h-full lg:mt-5 align-center bg-base-100 py-5">
+        <div>
+          <div className="flex flex-col text-center items-center">
+            <h6 className="text-4xl font-bold mx-2 mb-6">Easily find <span className="text-primary-focus">volunteering events of your interests</span> in your area!</h6>
+            {/* <p className="py-6 text-2xl">Easily find </p> */}
+            <div className="form-control">
+              <div className="input-group">
+                <input type="text" placeholder="Search…" className="input input-bordered md:w-96 " />
+                <button className="btn btn-square">
+                  <IoSearch className="h-6 w-6" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
