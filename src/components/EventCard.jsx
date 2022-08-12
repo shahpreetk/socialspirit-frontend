124ffb4771db226c05ebf38ffe6f66406875fd3c
Preet Shah <shahpreetk@gmail.com>
2022-08-12 19:40:37 +0100

    error trying to register when not logged in

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ALL_EVENTS } from "../constants/routes";
 import { toast } from "react-toastify";
-import { useSelector  } from "react-redux";
+import { useSelector } from "react-redux";
 
 const EventCard = ({ events }) => {
 
   const { organisation } = useSelector((state) => state.organisationauth);
+  const { volunteer } = useSelector((state) => state.volunteerauth);
+
+  const registerInterest = ( name) => {
+    if (volunteer) {
+      toast.success("Registered for " + name + " successfully");
+    } else {
+      toast.error("Please login to register for an event");
+    }
+  };
+
 
   return (
     <>
@@ -17,34 +27,34 @@ const EventCard = ({ events }) => {
               key={event._id}
               className="col-span-12 md:col-span-5 lg:col-span-4"
             >
-            <div className="card w-auto bg-base-100 shadow-md border-2 border-base-200 mx-6 md:mx-0">
-              <div className="card-body grid grid-flow-col p-4 items-baseline">
-                <Link to={`${ALL_EVENTS}/${event._id}`} className="hover:underline">
-                  <h2 className="card-title">{event.name}</h2>
-                </Link>
-                <p className="text-right align-middle">{new Date(event.date).toString().slice(0, 21)}</p>
-              </div>
-              <figure><img src={`https://placeimg.com/400/22${i}/arch`} alt={event.name} /></figure>
-              <div className="card-body p-4">
-                <p className="text-sm">{event.city}, {event.districtCode}</p>
-                <div className="flex items-center text-lg">
-                  <div className="avatar placeholder">
-                    <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
-                      <span className="text-sm">{event.ownerName.slice(0, 1)}</span>
-                    </div>
-                  </div> <p className="ml-2 md:text-lg text-md">{event.ownerName}</p></div>
-
-                <div className="card-actions justify-start mt-2">
-                  {event.tags.map((tag, i) => (
-                    <div key={i} className="badge badge-outline">#{tag}</div>
-                  ))}
+              <div className="card w-auto bg-base-100 shadow-md border-2 border-base-200 mx-6 md:mx-0">
+                <div className="card-body grid grid-flow-col p-4 items-baseline">
+                  <Link to={`${ALL_EVENTS}/${event._id}`} className="hover:underline">
+                    <h2 className="card-title">{event.name}</h2>
+                  </Link>
+                  <p className="text-right align-middle">{new Date(event.date).toString().slice(0, 21)}</p>
                 </div>
-               {!organisation && <div className="card-actions justify-end mt-2 md:mt-0">
-                  <button className="btn btn-primary" onClick={(e) => toast.success("Registered for " + event.name + " successfully")}>Register Interest!</button>
-                </div>}
+                <figure><img src={`https://placeimg.com/400/22${i}/arch`} alt={event.name} /></figure>
+                <div className="card-body p-4">
+                  <p className="text-sm">{event.city}, {event.districtCode}</p>
+                  <div className="flex items-center text-lg">
+                    <div className="avatar placeholder">
+                      <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
+                        <span className="text-sm">{event.ownerName.slice(0, 1)}</span>
+                      </div>
+                    </div> <p className="ml-2 md:text-lg text-md">{event.ownerName}</p></div>
+
+                  <div className="card-actions justify-start mt-2">
+                    {event.tags.map((tag, i) => (
+                      <div key={i} className="badge badge-outline">#{tag}</div>
+                    ))}
+                  </div>
+                  {!organisation && <div className="card-actions justify-end mt-2 md:mt-0">
+                    <button className="btn btn-primary" onClick={e => registerInterest(event.name)}>Register Interest!</button>
+                  </div>}
+                </div>
               </div>
-              </div>
-              </div>
+            </div>
           )}
       </div>
     </>
